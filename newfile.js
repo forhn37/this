28329f7b5b56afc22d1ded35cb4b4b4558ed569b
kimhyun <forhn37@gmail.com>
2023-10-11 11:06:03 +0900

    생성자 함수의 예시

--- newfile.js
+++ newfile.js
@@ -2,5 +2,13 @@ function student(name, number) {
   this.name = name; 
   this.number = number;
 }
-let test = new student("공욱재", "0")
-console.log(test);
+
+let list = ["이민구", "윤준현"];
+let studentArr = [];
+
+for(let i = 0; i < list.length; i++) {
+  studentArr[i] = new student(list[i], i+1)
+};
+
+
+console.log(studentArr);
