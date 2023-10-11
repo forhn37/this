ec58759e16b2cd3bf18f70eb0e260138ff8e40bd
kimhyun <forhn37@gmail.com>
2023-10-11 11:47:11 +0900

    arrow함수 예제 완성

--- arrow.js
+++ arrow.js
@@ -10,5 +10,20 @@ const outerTwo = function() {
 // 함수리터럴
 // 변수는 쓰지 않으면 메모리에서 없어짐
 
+const outerThree = () => "wow";
+const outerFour = function() {
+  return "wow"
+};
+// arrow함수의 형식 예제; three와 Four는 같다
+
+const outerFive = a => a;
+const outerSix = function(a) {
+  return a;
+}
 console.log(outer());
 console.log(outerTwo());
+console.log(outerThree());
+console.log(outerFour());
+console.log(outerFive());
+// 함수의 파라미터가 입력되지 않아 undefined로 됨
+console.log(outerSix(5));
